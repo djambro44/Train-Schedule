f890b357f1d431503c932330f739c3cbfd33321c
Dylan Ambrose <djambro44@gmail.com>
2020-03-19 19:51:06 -0700

    added firebase database

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,29 @@
-$("document").ready(function(){
+$("document").ready(function () {
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyAMaruBdafi8L6tealfJUeWeOMqCagVc2E",
+        authDomain: "da-bootcamp-trainschedule.firebaseapp.com",
+        databaseURL: "https://da-bootcamp-trainschedule.firebaseio.com",
+        projectId: "da-bootcamp-trainschedule",
+        storageBucket: "da-bootcamp-trainschedule.appspot.com",
+        messagingSenderId: "169963651588",
+        appId: "1:169963651588:web:fcaad259df50baf536ab51"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
 
 
 
 
-    
     //Define Variables - make this local inside of a function
     //Train Name
     //Destination
     //Frequency
     //next arrival
     //min away
-    
+
     //function for getting user input to add a new train
-    function getuserInput(){
+    function getuserInput() {
         var trainName = $("#trainNameInput").val().trim();
         var destination = $("#destinationInput").val().trim();
         var trainTime = $("#firstTrainTimeInput").val().trim();
@@ -24,21 +35,38 @@ $("document").ready(function(){
     }
 
     //make function to calculate min away
-    function calculateMinAway(){
+    function calculateMinAway() {
 
     }
 
-    function calculateNextArrival(){
+    //make function to calculate min away
+    function calculateNextArrival() {
 
     }
-    
-    //submit button on click function
-    $("#addTrainButton").on("click", function(trainName){
+
+    database.ref().set({
+        newName: trainName
+      });
+    database.ref().on("value", function(snapshot) {
+
+        // Then we console.log the value of snapshot
+        console.log(snapshot.val());
+  
+        // Update the clickCounter variable with data from the database.
+        trainName = snapshot.val();
+  
+        // Then we change the html associated with the number.
+  
+        // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+        // Again we could have named errorObject anything we wanted.
+      }
+
+    //submit button on click function that send user input to html and the answer to both calculations 
+    $("#addTrainButton").on("click", function (trainName) {
         getuserInput();
-        $('tbody').html('<tr>', trainName, '</tr>');
+        $('#timeTable > tbody:last-child').append('<tr>trainName</tr><tr>...</tr>');
 
 
     })
-    //send user inputs for name, destination, and frequency to table
-    //calculate next arrival time by 
+   
 })
