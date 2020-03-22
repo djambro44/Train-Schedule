b8ab733ea085501057bef7fed27f23405fe1a87c
Dylan Ambrose <djambro44@gmail.com>
2020-03-22 15:55:51 -0700

    Connected Firebase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,114 @@
 $("document").ready(function () {
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyAMaruBdafi8L6tealfJUeWeOMqCagVc2E",
-        authDomain: "da-bootcamp-trainschedule.firebaseapp.com",
-        databaseURL: "https://da-bootcamp-trainschedule.firebaseio.com",
-        projectId: "da-bootcamp-trainschedule",
-        storageBucket: "da-bootcamp-trainschedule.appspot.com",
-        messagingSenderId: "169963651588",
-        appId: "1:169963651588:web:fcaad259df50baf536ab51"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+  var firebaseConfig = {
+      apiKey: "AIzaSyAMaruBdafi8L6tealfJUeWeOMqCagVc2E",
+      authDomain: "da-bootcamp-trainschedule.firebaseapp.com",
+      databaseURL: "https://da-bootcamp-trainschedule.firebaseio.com",
+      projectId: "da-bootcamp-trainschedule",
+      storageBucket: "da-bootcamp-trainschedule.appspot.com",
+      messagingSenderId: "169963651588",
+      appId: "1:169963651588:web:fcaad259df50baf536ab51"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
 
 
+    var database = firebase.database();
 
-    //Define Variables - make this local inside of a function
-    //Train Name
-    //Destination
-    //Frequency
-    //next arrival
-    //min away
+  //Define Variables - make this local inside of a function
+  //Train Name
+  //Destination
+  //Frequency
+  //next arrival
+  //min away
 
-    //function for getting user input to add a new train
-    function getuserInput() {
-        var trainName = $("#trainNameInput").val().trim();
-        var destination = $("#destinationInput").val().trim();
-        var trainTime = $("#firstTrainTimeInput").val().trim();
-        var frequency = $("#frequencyInput").val().trim();
-        console.log(trainName);
-        console.log(destination);
-        console.log(trainTime);
-        console.log(frequency);
-    }
 
-    //make function to calculate min away
-    function calculateMinAway() {
-
-    }
-
-    //make function to calculate min away
-    function calculateNextArrival() {
-
-    }
-
+  //function for getting user input to add a new train
+  function getuserInput() {
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var trainTime = $("#firstTrainTimeInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(frequency);
     database.ref().set({
-        newName: trainName
-      });
-    database.ref().on("value", function(snapshot) {
-
-        // Then we console.log the value of snapshot
-        console.log(snapshot.val());
+      name: trainName,
+      destination: destination,
+      time: trainTime,
+      frequency: frequency
+    });
   
-        // Update the clickCounter variable with data from the database.
-        trainName = snapshot.val();
-  
-        // Then we change the html associated with the number.
-  
-        // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-        // Again we could have named errorObject anything we wanted.
-      }
+    return {
+      name: trainName,
+      destination: destination,
+      time: trainTime,
+      frequency: frequency
+    };
+  }
 
-    //submit button on click function that send user input to html and the answer to both calculations 
-    $("#addTrainButton").on("click", function (trainName) {
-        getuserInput();
-        $('#timeTable > tbody:last-child').append('<tr>trainName</tr><tr>...</tr>');
+  function buildTrainRow(train) {
+    // console.log("this exists in buildTrainRow: ", train)
+    $("#timeTable").append(`<tr>
+      <td>${train.name}</td>
+      <td>${train.destination}</td>
+      <td>${train.frequency}</td>
+      <td>Math Stuff</td>
+      <td>More Math Stuff</td>
+    </tr>`)
+  }
+
+  //make function to calculate min away
+  function calculateMinAway(hour , min) {
+    var minAway;
 
 
-    })
-   
+
+
+    return minAway;
+  }
+
+  //make function to calculate min away
+  function calculateNextArrival() {
+    var nextArrival;
+
+
+
+
+
+
+    return nextArrival;
+  }
+
+
+  // database.ref().set({
+  //     newName: trainName
+  //   });
+  // database.ref().on("value", function(snapshot) {
+
+  //     console.log(snapshot.val());
+
+
+  //     trainName = snapshot.val();
+
+
+  //   }
+
+  //submit button on click function that send user input to html and the answer to both calculations 
+  $("#addTrainButton").on("click", function () {
+    var newTrain = getuserInput();
+    
+    var time = moment().format('h:mm');
+    var splitTime = time.split(":");
+    var hour = parseInt(splitTime[0]);
+    var min = parseInt(splitTime[1]);
+    console.log(hour , min);
+    newTrain.minAway = calculateMinAway(hour , min);
+    
+    
+    buildTrainRow(newTrain);
+  })
+
 })
